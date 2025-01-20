b4728077a9805094cf358541309738c1c863b4e5
ReggelCamp <reggel2001@gmail.com>
2025-01-20 14:25:30 +0800

    practice deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { Navbar } from "@nextui-org/react";
-import Image from "next/image";
-//import Page from "./dashboard/page";
 import Dashboard from "./_sections/dashboard";
 
 export default function Home() {
-  // return <Page />;
   return <Dashboard />;
 }
